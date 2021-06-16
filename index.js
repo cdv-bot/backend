99765ac31942f33ae340446107b7f88dda8e7b6c
do.an <doan.cdv@gmail.com>
2021-06-16 13:22:15 +0700

    ád

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-	const app = require('express')()
+const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http,{ origins: '*:*'})
+const PORT = process.env.PORT || 3000;
 
 io.on('connection', socket => {
   socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message })
+    io.emit('message', { name, message:"áds" })
   })
 })
 
-http.listen(4000, function() {
+http.listen(PORT, function() {
   console.log('listening on port 4000')
 })
