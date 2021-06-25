80f52ee291c3c8a4c8484a668ef159280462bdb3
do.an <doan.cdv@gmail.com>
2021-06-25 17:09:48 +0700

    ád

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http,{ origins: '*:*'})
-const PORT = process.env.PORT || 3000;
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { origins: '*:*' });
+const PORT = process.env.PORT || 4000;
+const request = require('request');
+var rp = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 io.on('connection', socket => {
-  socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message:"áds" })
-  })
-})
+  socket.on('message', async ({ name, message }) => {
+    io.emit('message', { name, message });
+  });
+});
 
-http.listen(PORT, function() {
-  console.log('listening on port 4000')
-})
+http.listen(PORT, function () {
+  console.log('listening on port 4000');
+});
