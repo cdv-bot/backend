4e5125bb748c53edc1f930d5510ffc2a1ae3ac92
do.an <doan.cdv@gmail.com>
2021-06-28 13:25:39 +0700

    asd

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const app = require('express')();
-const http = require('http').createServer(app);
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
 const cors = require('cors');
-const io = require('socket.io')(http, { origins: '*:*' });
 const PORT = process.env.PORT || 4000;
-const request = require('request');
-var rp = require('request-promise');
-const cheerio = require('cheerio');
-const fs = require('fs');
+
+const app = express();
+app.get('/',(req,res)=> res.send("hello world"));
+const server = http.Server(app);
+server.listen(PORT);
+const io = socket(server);
+
 app.use(cors());
+
 io.on('connection', socket => {
   socket.on('message', async ({ name, message }) => {
     io.emit('message', { name, message });
   });
 });
 
-http.listen(PORT, function () {
-  console.log('listening on port 4000');
-});
+
