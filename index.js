401b08c5b48e17d9a9fd3d8de85d2dbef8175d57
do.an <doan.cdv@gmail.com>
2021-06-28 11:28:16 +0700

    asd

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const express = require('express');
+const app = require('express')();
 const http = require('http').createServer(app);
 const cors = require('cors');
-const socketio = require('socket.io');
-const http = require("http");
+const io = require('socket.io')(http, { origins: '*:*' });
 const PORT = process.env.PORT || 4000;
-
-
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-
+const request = require('request');
+var rp = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
 app.use(cors());
-
-app
 io.on('connection', socket => {
   socket.on('message', async ({ name, message }) => {
     io.emit('message', { name, message });
