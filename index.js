136f7eefe1c6cb5d802a92f9f6a8299b9eea9a60
do.an <doan.cdv@gmail.com>
2021-07-23 22:21:22 +0700

    Ad

--- index.js
+++ index.js
@@ -2,25 +2,32 @@ const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
 const cors = require('cors');
+const Message = require('./src/models');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-app.get('/',(req,res)=> res.send("hello world"));
-const server = http.Server(app);
-server.listen(PORT);
-const io = socket(server,{
-  cors:{
-    origin:"*",
-    methods:["GET","POST"]
-  }
-});
+
+const db = require('./src/connect');
+const { Mongoose } = require('mongoose');
+db.connect();
 
 app.use(cors());
-
-io.on('connection', socket => {
-  socket.on('message', async ({ name, message }) => {
-    io.emit('message', { name, message });
+app.get('/', (req, res) => {
+  Message.find({}, (err, docs) => {
+    if (!err) {
+      res.json(docs);
+      return;
+    }
+    res.status(400).json({ error: 'error' });
   });
 });
-
+const server = http.Server(app);
+server.listen(PORT);
+const io = socket(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
 
